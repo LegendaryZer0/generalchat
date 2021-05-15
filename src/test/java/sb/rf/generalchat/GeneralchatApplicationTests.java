18e1893f94d86f23584cf959d0b6daa15b1648fd
Rifdar <assasinnyx@mail.ru>
2021-05-15 11:53:42 +0300

    added port to prod properties, modified tests

--- src/test/java/sb/rf/generalchat/GeneralchatApplicationTests.java
+++ src/test/java/sb/rf/generalchat/GeneralchatApplicationTests.java
@@ -3,9 +3,10 @@ package sb.rf.generalchat;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import java.util.function.Function;
-
+@WebAppConfiguration
 @SpringBootTest
 class GeneralchatApplicationTests {
     Function<Integer, Boolean> numberToBooleanMapper = o -> {
